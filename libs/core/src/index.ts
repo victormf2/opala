2f3a23cdebb9ca1cf1755995d4b902eec251082c
Victor Machado <victor_mf2@hotmail.com>
2023-12-10 13:49:02 -0300

    removing old packages

--- libs/core/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './create';
-export * from './operation';
-export * from './operation-builder';
-export * from './query';
-export * from './schema';
--- libs/core/src/operation.ts
+++
@@ -1,5 +0,0 @@
-export abstract class Operation<TInput, TOutput> {
-  // Branding
-  private readonly _input?: TInput;
-  private readonly _output?: TOutput;
-}
